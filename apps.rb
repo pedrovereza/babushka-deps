2f5b03b61176645d6ead2ebab037ece718547c50
Pedro Vereza <pdrvereza@gmail.com>
2016-03-13 13:01:42 -0300

    Adds Chrome

--- apps.rb
+++ apps.rb
@@ -1,3 +1,7 @@
+dep  'Chrome.app' do
+  source 'https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg'
+end
+
 dep 'iTerm2.app' do
   source 'https://iterm2.com/downloads/stable/iTerm2_v2_0.zip'
 end 
